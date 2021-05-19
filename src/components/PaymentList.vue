d244ed2da16c61128a809e10aadda7aebe9cfbc5
Vitaly <vitalii.a.sh@mail.ru>
2021-05-19 20:58:45 +0300

    lesson3

---
+++ src/components/PaymentList.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="listWrap">
+        <div class="list headerList">
+            <div> # </div>
+            <div> Date </div>
+            <div> Category </div>
+            <div> Cost </div>
+        </div>
+        <div class="list" v-for="(item, index) in items" :key="index">
+            <div> {{ index + 1 }} </div>
+            <div> {{ item.date }} </div>
+            <div> {{ item.category }} </div>
+            <div> {{ item.price }} </div>
+        </div>
+        <div class="diagram">
+            <img alt="Vue logo" src="../assets/D-bZe1wUEAIGOng.png" />
+            </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        items: Array
+    },
+    methods: {
+        doSomething () {
+            console.log(this.items);
+        }
+    }
+
+}
+</script>
+
+<style>
+.list {
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+    font-size: 15px;
+    font-weight: 400;
+    text-align: start;
+    margin: 20px;
+    width: 800px;
+    display: grid;
+    grid-template-rows: 40px;
+    grid-template-columns: 10% 30% 30% 30%;
+    border-bottom: 2px solid rgb(168, 166, 166);
+}
+
+.headerList {
+    border-bottom: 3px solid rgb(185, 72, 57);
+    font-weight: 600;
+    color:rgb(185, 72, 57);
+}
+
+.diagram {
+    position: absolute;
+    right: 0;
+}
+
+.listWrap {
+    position: relative;
+    display: grid;
+}
+
+img {
+        height: 400px;
+    width: 400px;
+}
+</style>
